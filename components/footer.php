26d72f928481651dcbed7ea487869cceabe743c1
João Mesquita <contatomesquita@outlook.com>
2021-08-13 11:53:59 -0300

    Configuração inicial dos arquivos

---
+++ components/footer.php
@@ -0,0 +1,31 @@
+    <div class="footer bg-gray p-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6 text-center">
+                    <img src="img/logo-footer.png" alt="Aquario Transparente" class="img-fluid me-5">
+                    <img src="img/logo-ms-footer.svg" alt="Governo do Estado do Mato Grosso do Sul" class="img-fluid" width="160">
+                </div>
+                <div class="col-12 col-lg-6 d-lg-flex">
+                    <p class="font-16 color-gray-alternative my-3 mb-lg-0 mt-lg-0">© Aquário Transparente Copyright 2021 by 8020MKT</p>
+                    
+                    <div class="social text-center ms-lg-5">
+                        <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none me-2">
+                            <i class="bi bi-twitter"></i>
+                        </a>
+                        <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none me-2">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                        <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Aquario Transparente</title>
+</head>
+<body>
+    <div class="bg-light p-2">
+        <div class="container border-bottom">
+            <div class="d-flex flex-row justify-content-between">
+                <div class="local d-flex">
+                    <i class="bi bi-geo-alt-fill color-red me-2"></i>
+                    <p class="font-14 color-gray">Campo Grande, MS - BR</p>
+                </div>
+
+                <div class="social">
+                    <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none me-2">
+                        <i class="bi bi-twitter"></i>
+                    </a>
+                    <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none me-2">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="http://" target="_blank" class="color-gray-alternative text-decoration-none">
+                        <i class="bi bi-instagram"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.png" alt="Aquário Transparente" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="apresentacao.php">Apresentação</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="retomada-obras.php">Retomada das Obras</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="licitacoes.php">Licitações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pesquisa-ciencia.php">Pesquisa e Ciência</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="galeria.php">Fotos e Vídeos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cadastro.php">Cadastro Imprensa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
